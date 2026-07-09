b70607e169977d7a4e5d5e80f66a12800e0cf6f5
Ajay A <kit28.24bcb001@gmail.com>
2026-07-09 20:30:06 +0530

    DAY 1 problems

---
+++ DAY-1/validpranthes.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }else{
+                if(st.size() == 0) return false;
+                
+                boolean case1 = c == ')' && st.peek() == '(';
+                boolean case2 = c == '}' && st.peek() == '{';
+                boolean case3 = c == ']' && st.peek() == '[';
+                if(case1 || case2 || case3){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.size() == 0) return true;
+        else return false;
+    }
+}
